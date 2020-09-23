91984772da3ccc9cb979bf1bf62597e82d43ed46
vladd <vladdyachkin11@gmail.com>
2020-09-23 15:22:47 +0300

    added solution

--- src/main/java/com/internet/shop/dao/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/internet/shop/dao/jdbc/UserDaoJdbcImpl.java
@@ -11,11 +11,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Dao
 public class UserDaoJdbcImpl implements UserDao {
