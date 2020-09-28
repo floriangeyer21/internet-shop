52655f9be3f4b3118b99d24b9911e5f2d387b774
vladd <vladdyachkin11@gmail.com>
2020-09-28 20:11:58 +0300

    temp commit

--- src/main/java/com/internet/shop/controllers/LoginController.java
+++ src/main/java/com/internet/shop/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.internet.shop.exceptions.AuthenticationException;
 import com.internet.shop.lib.Injector;
 import com.internet.shop.model.User;
 import com.internet.shop.security.AuthenticationService;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
